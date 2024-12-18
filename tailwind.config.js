bb598cf869cbee603c5a6dd0176366e9bb92faac
Axel Pointud <axel@leedpulse.com>
2024-12-18 12:34:26 +0100

    add hero and navigation (wip)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6533FF",
+        secondary: "#FBE4AB",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+        },
+      },
+    },
   },
   plugins: [],
 };
